71f38174566390be480abd49e62cfa1409bd65c4
Frédéric Jacques <gary.bob88@gmail.com>
2021-10-28 19:58:24 -0400

    ajout parametre k8s

---
+++ AzureDevOpsTeamMembersVelocity/Settings/KubernetesPageSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace AzureDevOpsTeamMembersVelocity.Settings
+{
+    /// <summary>
+    /// Settings for the kubernetes page
+    /// </summary>
+    public class KubernetesPageSettings : AbstractSettings
+    {
+        /// <summary>
+        /// Collapse the namespaces section
+        /// </summary>
+        public string? NamespaceCollapseClass { get; set; }
+
+        /// <summary>
+        /// Collapse the deployments section
+        /// </summary>
+        public string? DeploymentCollapseClass { get; set; }
+
+        /// <summary>
+        /// Collapse the pods section
+        /// </summary>
+        public string? PodsCollapseClass { get; set; }
+    }
+}
