ceb07b31d5e70a9a4fc8fbe5243350def91271ca
Frédéric Jacques <gary.bob88@gmail.com>
2021-10-31 20:26:08 -0400

    refactor kubernetes page

---
+++ UnitTest/KubernetesDashboardTest.cs
@@ -0,0 +1,15 @@
+﻿using AzureDevOpsTeamMembersVelocity.Pages;
+using UnitTest.AutoData;
+using Xunit;
+
+namespace UnitTest
+{
+    public class KubernetesDashboardTest
+    {
+        [Theory, AutoVelocityData]
+        public void Test(KubernetesDashboard sut)
+        {
+            Assert.NotNull(sut);
+        }
+    }
+}
